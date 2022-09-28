e77eb5eab5a83fc09d7d101934e76bc51c11fb25
Jeffrey Wallace <jcwallace002@gmail.com>
2022-09-28 10:49:25 -0400

    v5

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 function button1Clicked() {;
 
-    let buttonDiv = document.getElementById("button-1-div");
+    let button1Div = document.getElementById("button-1-div");
+    let button1 = document.getElementById("button1");
 
-    buttonDiv.innerHTML = "Button Pressed"; 
+    button1.remove;
+    button1Div.innerHTML = "<h1>Marks</h1><ul><li>Lines</li><li>Area</li></ul><h1>Channels</h1><ul><li>Position (Both)</li><ul><li>They use position to reflect the position of each state. This is a good choice and done effectively.</li></ul><li>Shape</li><ul><li>The shape of each state reflects the actual shape of each state. This is a good choice and is done effectively</li></ul><li>Length</li><ul><li>The length of the lines above the map reflects the number of votes each party has. This is a good choice and is done effectively. They use color as well to show where the length of each line cuts off.</li></ul><li>Color</li><ul><li>The color of each state represents (I assume, they did not define this) the political leaning of each state. This is a good choice because blue typically reflects democrat and red republican, however they do not clarify what the condition is for a state to be blue, red, or striped.</li></ul></ul>"; 
 }
 
 function button2Clicked() {;
 
-    let buttonDiv = document.getElementById("button-2-div");
+    let button2Div = document.getElementById("button-2-div");
+    let button2 = document.getElementById("button2");
 
-    buttonDiv.innerHTML = "Button Pressed"; 
+    button2.remove;
+    button2Div.innerHTML = "<h1>Colormap(s)</h1><ul><li>Divergent</li><ul><li>They use a divergent red to blue color map containing three possible colors: red, striped, then blue. I would characterize this as divergent because the striped represents a middleground between the red and blue. Each color represents the political leaning of the state (though unclear by what definition). This is a good choice because each color reflects the natural democrats are blue / republicans red tradition and both colors are easy to see.</li></ul></ul>"; 
 }
 
 
