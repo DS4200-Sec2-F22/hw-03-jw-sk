f461d703671eae5912026063af290a569c0a57a7
Jeffrey Wallace <jcwallace002@gmail.com>
2022-09-29 13:38:56 -0400

    vJW

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function button1Clicked() {;
     let button1 = document.getElementById("button1");
 
     button1.remove();
-    button1Div.innerHTML = "<h1>Marks</h1><ul><li>Lines</li><li>Area</li></ul><h1>Channels</h1><ul><li>Position (Both)</li><ul><li>They use position to reflect the position of each state. This is a good choice and done effectively.</li></ul><li>Shape</li><ul><li>The shape of each state reflects the actual shape of each state. This is a good choice and is done effectively</li></ul><li>Length</li><ul><li>The length of the lines above the map reflects the number of votes each party has. This is a good choice and is done effectively. They use color as well to show where the length of each line cuts off.</li></ul><li>Color</li><ul><li>The color of each state represents (I assume, they did not define this) the political leaning of each state. This is a good choice because blue typically reflects democrat and red republican, however they do not clarify what the condition is for a state to be blue, red, or striped.</li></ul></ul>"; 
+    button1Div.innerHTML = "<h1>Marks</h1><ul><li>Lines</li><li>Area</li></ul><h1>Channels</h1><ul><li>Position (Both)</li><ul><li>They use position to reflect the position of each state. This is a good choice and done effectively.</li></ul><li>Shape</li><ul><li>The shape of each state reflects the actual shape of each state. This is a good choice and is done effectively because each shape reflects the shape of the states themselves, which prevents the need to label each state. </li></ul><li>Length</li><ul><li>The length of the lines above the map reflects the number of votes each party has. This is a good choice and is done effectively. They use color as well to show where the length of each line cuts off.</li></ul><li>Color</li><ul><li>The color of each state represents (I assume, they did not define this) the political leaning of each state. This is a good choice because blue typically reflects democrat and red republican, however they do not clarify what the condition is for a state to be blue, red, or striped.</li></ul></ul>"; 
 }
 
 function button2Clicked() {;
@@ -23,7 +23,7 @@ function button2Clicked() {;
 
 
 
-
+I was thinking . Each shape needs to be unique in order to color them individually, so they must have had a way to code in the shape.
 
 
 
